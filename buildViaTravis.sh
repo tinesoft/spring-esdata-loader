5b2f8597600e21e118aeec821526d64b15c925b3
Tine Kondo <kondotine@gmail.com>
2019-07-29 06:25:16 +0200

    build(all): better modularize gradle build files

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -3,14 +3,12 @@
 
 SWITCHES="--info --stacktrace"
 
-GRADLE_VERSION=$(./gradlew -version | grep Gradle | cut -d ' ' -f 2)
-
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew build $SWITCHES
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew build snapshot $SWITCHES
+  ./gradlew artifactoryPublish $SWITCHES
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
@@ -27,9 +25,6 @@ else
 fi
 
 EXIT=$?
-
-rm -f "$HOME/.gradle/caches/modules-2/modules-2.lock"
-rm -rf "$HOME/.gradle/caches/$GRADLE_VERSION/plugin-resolution"
 
 exit $EXIT
 
