41ed3f6721d63ff27ae962ff65e563d4cb3073aa
Tine Kondo <kondotine@gmail.com>
2019-08-24 00:58:05 +0200

    chore(release): replace `nebula-release-plugin`  by `gradle-semantic-release-plugin`

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -1,30 +1,20 @@
+
 #!/bin/bash
 # This script will build the project.
 
 SWITCHES="--info --stacktrace"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew build $SWITCHES
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo -e 'Build Release'
+  ./gradlew publishPackageToBintray gitPublishPush semanticReleasePublish  $SWITCHES
+elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e 'Build Snapshot'
   ./gradlew artifactoryPublish $SWITCHES
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  case "$TRAVIS_TAG" in
-  *-rc\.*)
-    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true candidate gitPublishPush $SWITCHES
-    ;;
-  *)
-    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true final gitPublishPush $SWITCHES
-    ;;
-  esac
 else
-  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+    echo -e 'Build Branch ['$TRAVIS_BRANCH'] PR ['$TRAVIS_PULL_REQUEST']'
   ./gradlew build $SWITCHES
 fi
 
 EXIT=$?
 
 exit $EXIT
-
